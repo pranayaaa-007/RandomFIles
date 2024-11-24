408b34a6f20eebbd3eb69166588bc57aa2c7ab03
pranayaaa-007 <pranayaaa007@proton.me>
2024-11-24 16:25:55 +0545

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,1 +1,13 @@
+def main():
+    full_name = input("Enter the person's first, middle, and last name: ").strip().title()
+    output = get_initials(full_name)
+    print(f"Initials: {output}")
 
+
+def get_initials(full_name):
+    first_name, middle_name, last_name = full_name.split(" ")
+    return f"{first_name[0]}.{middle_name[0]}.{last_name[0]}."
+
+
+if __name__ == "__main__":
+    main()
